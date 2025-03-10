c7f9257e4cc0c510b939cdb452a8643f71636d99
Syrym <suleimenov.syrym.s@gmail.com>
2025-03-10 20:41:27 +0500

    Создание и редактирование пользователя

---
+++ app/Http/Controllers/Admin/UserSController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserSController extends Controller
+{
+    public function list(User $user)
+    {
+        $users = $user::where("role","NOT LIKE","admin")->paginate(15,['id','first_name','last_name','email','role']);
+
+        return response()->json($users);
+    }
+
+    public function item(User $user,Request $request)
+    {
+        $validated = Validator::make($request->all(),[
+            "id"=>"required|numeric",
+        ]);
+
+        $validated_data = $validated->safe()->only(['id']);
+
+        if(is_null($validated->errors())){
+            return response()->json(['status'=>false,'route'=>'back','reason'=>'where id']);
+        }
+
+        $id = $validated_data['id'];
+        $users = $user::where("role","NOT LIKE","admin")->where("id",$id)->first(['id','first_name','last_name','email','role',"password"]);
+        
+        if(is_null($users)){
+            return response()->json(['status'=>false,'route'=>'back','reason' => 'result is null']);
+        }
+
+        $users['status'] = true;
+        return response()->json($users);
+    }
+
+    public function create(User $user,Request $request)
+    {
+        $validated = Validator::make($request->all(),[
+            "first_name" => 'required|string',
+            "last_name" => 'required|string',
+            "email" => 'required|email|unique:mysql.users,email',
+            "role" => "required|exists:mysql.role_models,role",
+            "password" => "required|between:8,24"
+        ]);
+
+        $validated_data = $validated->safe()->only(["first_name","last_name","email","role","password"]);
+
+        if(is_null($validated->errors())){
+            return response()->json(['status'=>false,'route'=>'back','reason'=>'not validated']);
+        }
+
+        $first_name = $validated_data['first_name'];
+        $last_name = $validated_data['last_name'];
+        $email = $validated_data['email'];
+        $role = $validated_data['role'];
+        $password = Hash::make($validated_data['password']);
+
+        $user->first_name = $first_name;
+        $user->last_name = $last_name;
+        $user->email = $email;
+        $user->role = $role;
+        $user->password = $password;
+
+        $user->save();
+
+        return response()->json(['status'=>true]);
+    }
+
+    public function update(User $user,Request $request)
+    {
+        $validated = Validator::make($request->all(),[
+            "id" => "required|numeric|exists:mysql.users,id",
+            "first_name" => 'string',
+            "last_name" => 'string',
+            "email" => 'email|unique:mysql.users,email',
+            "role" => "exists:mysql.role_models,role",
+            "password" => "between:8,24"
+        ]);
+
+        $validated_data = $validated->safe()->only(["id","first_name","last_name","email","role","password"]);
+
+        if(is_null($validated->errors())){
+            return response()->json(['status'=>false,'route'=>'back','reason'=>'not validated']);
+        }
+
+        $id = $validated_data['id'];
+        $user = $user::where('role',"NOT LIKE","admin")->where("id",$id);
+
+        isset($validated_data['first_name']) ?: $user->first_name = $validated_data['first_name'];
+        isset($validated_data['last_name']) ?: $user->last_name = $validated_data['last_name'];
+        isset($validated_data['email']) ?: $user->email = $validated_data['email'];
+        isset($validated_data['role']) ?: $user->role = $validated_data['role'];
+        isset($validated_data['password']) ?: $user->email = Hash::make($validated_data['password']);
+
+        $user->save();
+
+        return response()->json(['status'=>true]);
+    }
+
+    public function delete(User $user,Request $request)
+    {
+        $validated = Validator::make($request->all(),[
+            "id" => "required|numeric|exists:mysql.users,id"
+        ]);
+
+        $validated_data = $validated->safe()->only(["id"]);
+
+        if(is_null($validated->errors())){
+            return response()->json(['status'=>false,'route'=>'back','reason'=>'not validated']);
+        }
+
+        $id = $validated_data['id'];
+        $user = $user::where('role',"NOT LIKE","admin")->where("id",$id);
+
+        $user->delete();
+
+        return response()->json(['status'=>true]);
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -1,3 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserSController;
+use App\Http\Middleware\AsyncAdminMiddleware;
+
+Route::controller(UserSController::class)->group(function(){
+    Route::post("/users/list","list")->middleware(AsyncAdminMiddleware::class)->name('users.list');
+    Route::post("/users/item","item")->middleware(AsyncAdminMiddleware::class)->name('users.item');
+    Route::post("/users/create","create")->middleware(AsyncAdminMiddleware::class)->name('users.create');
+    Route::post("/users/update","update")->middleware(AsyncAdminMiddleware::class)->name('users.update');
+    Route::post("/users/delete","delete")->middleware(AsyncAdminMiddleware::class)->name('users.delete');
+});
