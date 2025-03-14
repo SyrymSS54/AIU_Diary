5446d61cde950bb22dc7dbe56cb676a6bb8bcaea
Syrym <suleimenov.syrym.s@gmail.com>
2025-03-14 14:29:52 +0500

    Добавление маршрутов

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\CursController;
 use App\Http\Controllers\Admin\OrganizationController;
+use App\Http\Controllers\Admin\ProgramController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserSController;
 use App\Http\Middleware\AsyncAdminMiddleware;
@@ -19,4 +21,20 @@ Route::controller(OrganizationController::class)->group(function(){
     Route::post("/org/create","create")->middleware(AsyncAdminMiddleware::class)->name("org.create");
     Route::post("/org/update","update")->middleware(AsyncAdminMiddleware::class)->name("org.update");
     Route::post("/org/delete","delete")->middleware(AsyncAdminMiddleware::class)->name("org.delete");
+});
+
+Route::controller(ProgramController::class)->group(function(){
+    Route::post("/program/list",'list')->middleware(AsyncAdminMiddleware::class)->name('program.list');
+    Route::post("/program/item",'item')->middleware(AsyncAdminMiddleware::class)->name('program.item');
+    Route::post("/program/create",'create')->middleware(AsyncAdminMiddleware::class)->name('program.create');
+    Route::post("/program/update",'update')->middleware(AsyncAdminMiddleware::class)->name('program.update');
+    Route::post("/program/delete",'delete')->middleware(AsyncAdminMiddleware::class)->name('program.delete');
+});
+
+Route::controller(CursController::class)->group(function(){
+    Route::post("/curs/list",'list')->middleware(AsyncAdminMiddleware::class)->name('curs.list');
+    Route::post("/curs/item",'item')->middleware(AsyncAdminMiddleware::class)->name('curs.item');
+    Route::post("/curs/create",'create')->middleware(AsyncAdminMiddleware::class)->name('curs.create');
+    Route::post("/curs/update",'update')->middleware(AsyncAdminMiddleware::class)->name('curs.update');
+    Route::post("/curs/delete",'delete')->middleware(AsyncAdminMiddleware::class)->name('curs.delete');
 });
